22b1b6c59d418720495b5b7f5d1f1afd05d59af2
iamspl <shivaprasad.l@outlook.com>
2019-12-11 20:05:38 -0600

    Spring Project

---
+++ src/com/luv2code/springdemo/TrackCoach.java
@@ -0,0 +1,31 @@
+package com.luv2code.springdemo;
+
+public class TrackCoach implements Coach {
+	
+	private FortuneService fortuneService;
+	
+	public TrackCoach(){
+		
+	}
+
+	/**
+	 * @param fortuneService
+	 */
+	public TrackCoach(FortuneService fortuneService) {
+		super();
+		this.fortuneService = fortuneService;
+	}
+
+	@Override
+	public String getDailyWorkout() {
+		// TODO Auto-generated method stub
+		return "Run a hard 5k";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		
+		return "just do it: " + fortuneService.getFortune();
+	}
+
+}
